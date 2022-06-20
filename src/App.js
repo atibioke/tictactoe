266ae42331fcf7256c5e3d38fee4ae11addcd782
Rasaq Ganiyu <eudoxusganium@gmail.com>
2022-06-20 08:29:16 +0100

    working on the functionality of my tictactoe game

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import SignIn from "../src/components/sign-in/SignIn";
+// import SignIn from "../src/components/sign-in/SignIn";
 
 import "./App.css";
-// import TicTacToe from "./components/tictactoe-container/TicTacToe";
+import TicTacToe from "./components/tictactoe-container/TicTacToe";
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <TicTacToe />
     </div>
   );
 }
--- src/components/tictactoe-container/TicTacToe.js
+++ src/components/tictactoe-container/TicTacToe.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TicTacToe.css";
 
 const TicTacToe = () => {
+  // const [ startGame, setStartGame] = useState(false);
+  const [gameValue, setGameValue] = useState("");
+
+  const handleClick = (index) => {
+    if (index !== -1) {
+      if (gameValue === "X") {
+        setGameValue("O");
+      } else {
+        setGameValue("X");
+      }
+    }
+  };
+
+  // if(startGame){
+
+  // }
+
   return (
     <React.Fragment>
       <section className="game-container">
         <section className="game-inner-container">
-          <div className="grid-hole"></div>
-          <div className="grid-hole"></div>
-          <div className="grid-hole"></div>
-          <div className="grid-hole"></div>
-          <div className="grid-hole"></div>
-          <div className="grid-hole"></div>
-          <div className="grid-hole"></div>
-          <div className="grid-hole"></div>
-          <div className="grid-hole"></div>
+          {Array(9)
+            .fill(0)
+            .map((box, index) => {
+              return (
+                <div
+                  key={index}
+                  className="grid-hole"
+                  onClick={() => handleClick(index)}
+                >
+                  {gameValue}
+                </div>
+              );
+            })}
         </section>
       </section>
+      {/* <button onClick={()=> setStartGame(true)}>restart</button> */}
     </React.Fragment>
   );
 };
