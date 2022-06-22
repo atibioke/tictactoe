f4b63aded5e6e96151b44fbbb25c1a5a4ea7513a
Rasaq Ganiyu <eudoxusganium@gmail.com>
2022-06-22 19:55:08 +0100

    working on form validation

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -32,8 +32,8 @@ const SignUp = () => {
     if (
       username &&
       password &&
-      /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email) &&
-      !validData
+      /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)
+      
     ) {
       setUsername(username);
       setPassword(password);
